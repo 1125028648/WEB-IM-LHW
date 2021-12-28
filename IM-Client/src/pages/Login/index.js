50d30d8e561ca7f9cfc6ab2696e5069ba698788b
Henry <huangxin21@mail.ustc.edu.cn>
2021-12-29 00:33:17 +0800

    12.29

--- IM-Client/src/pages/Login/index.js
+++ IM-Client/src/pages/Login/index.js
@@ -10,10 +10,9 @@ export default class Login extends Component  {
             login: false
         }
     }
-
+    // 提交成功
     onFinish = (values) => {
         values.password = this.$md5(values.password);
-        // console.log(values.password)
 
         this.$axios.post("/login", values).then(
             res => {
@@ -22,15 +21,17 @@ export default class Login extends Component  {
                         login: true
                     });
                 }else{
-                    // 登录失败提示, 这里加个提示弹窗
+                    // 登录失败提示
                     console.log(res.data.data);
                     console.log(res.data.message);
+                    alert('登录失败o(╥﹏╥)o，请稍后重试！');
                 }
             });
     };
-
+    // 提交失败
     onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
+        alert("提交错误！");
     }
     
     render(){
@@ -38,54 +39,55 @@ export default class Login extends Component  {
             return <Navigate to='/homePage'/>
         }
         return (
-            <div className='loginFrame'>
-                <h1 style={{textAlign: "center",fontWeight: 'bolder', color: "#1890ff"}}>系统登录</h1>
-                <Form
-                    name='login' 
-                    labelCol = {{span: 8,}} 
-                    wrapperCol={{span: 16}} 
-                    // initialValues={{remember: true,}}
-                    onFinish={this.onFinish}
-                    onFinishFailed={this.onFinishFailed}
-                    autoComplete="off"
-                >
-                    <Form.Item
-                        label="电子邮箱"
-                        name="email"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your email!',
-                            },
-                        ]}
+            <div className='background'>
+                <div className='loginFrame'>
+                    <h1 style={{textAlign: "center",fontWeight: 'bolder', color: "#1890ff"}}>系统登录</h1>
+                    <Form
+                        name='login' 
+                        labelCol = {{span: 8,}} 
+                        wrapperCol={{span: 16}} 
+                        // initialValues={{remember: true,}}
+                        onFinish={this.onFinish}
+                        onFinishFailed={this.onFinishFailed}
+                        autoComplete="off"
                     >
-                        <Input className='input' />
-                    </Form.Item>
-                    <Form.Item
-                        label="密码"
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your password!',
-                            },
-                        ]}
+                        <Form.Item
+                            label="电子邮箱"
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your email!',
+                                },
+                            ]}
+                        >
+                            <Input className='input' />
+                        </Form.Item>
+                        <Form.Item
+                            label="密码"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password className='input' />
+                        </Form.Item>
+                        <Form.Item
+                            wrapperCol={{
+                                offset: 8,
+                                span: 16,
+                            }}
                     >
-                        <Input.Password className='input' />
-                    </Form.Item>
-                    <Form.Item
-                        wrapperCol={{
-                            offset: 8,
-                            span: 16,
-                        }}
-                >
-                    <Button type="primary" htmlType="submit">
-                        登录
-                    </Button>
-                    </Form.Item>
-                </Form>
+                        <Button type="primary" htmlType="submit">
+                            登录
+                        </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
-        
         )
     }  
 }
--- IM-Client/src/pages/Welcome/index.js
+++ IM-Client/src/pages/Welcome/index.js
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
+import './index.css'
 
 const Welcome = () => {
     return (
-        <div>
+        <div className='background'>
             <h1 style={{textAlign:"center", marginTop: "10%"}}>Welcome !</h1>
             <br/>
             <Link to='/login' style={{fontSize: "24px", marginLeft: "37%", color: "blue"}}>登录首页</Link>
